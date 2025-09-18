5b12dd8c744268b7fe553ee7ee1c31c26c132c36
mthunbo <mthunbov2@gmail.com>
2025-09-18 14:57:10 +0200

    refactor(animation): optimize embers with CSS animations (#6)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./Animations/**/*.{js,ts,jsx,tsx}",
   ],
   safelist: [
     'bg-primary',
